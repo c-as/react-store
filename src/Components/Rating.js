02bf66c43f8140dc4bf33ca33988effc1e9d6b1c
cbarsugman <casbrugman@hotmail.nl>
2021-02-23 16:50:39 +0100

    Created component to show rating

---
+++ src/Components/Rating.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import PropTypes from "prop-types"
+import FullStar from "../star_full.svg"
+import HalfStar from "../star_half.svg"
+import EmptyStar from "../star_empty.svg"
+
+export default class Rating extends Component {
+  render() {
+    let stars = []
+    let current = this.props.score
+    for (let i = 0; i < 5; i++) {
+      if (current >= 1) {
+        stars.push(<img src={FullStar} alt="" key={i} />)
+        current--
+      } else if (current === 0.5) {
+        stars.push(<img src={HalfStar} alt="" key={i} />)
+        current--
+      } else {
+        stars.push(<img src={EmptyStar} alt="" key={i} />)
+      }
+
+    }
+    return (
+      <div className="rating">
+        {stars}
+      </div>
+    )
+  }
+}
+
+Rating.defaultProps = {
+  score: PropTypes.number
+}
+
