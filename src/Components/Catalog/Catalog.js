585a929bb714ac45cb6e0cfbb1399caaf677a444
cbarsugman <casbrugman@hotmail.nl>
2021-02-24 23:36:28 +0100

    No need to be components

--- src/Components/Catalog/Catalog.js
+++ src/Components/Catalog/Catalog.js
@@ -1,33 +1,31 @@
-import React, { Component } from "react"
+import React from "react"
 import PropTypes from "prop-types"
 import Item from "./Item"
 
-export default class Catalog extends Component {
-  render() {
-    let items = []
-    this.props.items.forEach((item) => {
-      items.push(
-        <Item
-          id={item._id}
-          name={item.name}
-          price={item.price}
-          rating={item.avgRating}
-          isOnSale={item.isOnSale}
-          image={item.imageUrl}
-          key={item._id}
-        />
-      )
-    })
-    return (
-      <div className="catalog">
-        {items.length > 0 ? (
-          items
-        ) : (
-          <div className="message">{this.props.message}</div>
-        )}
-      </div>
+export default function Catalog(props) {
+  let items = []
+  props.items.forEach((item) => {
+    items.push(
+      <Item
+        id={item._id}
+        name={item.name}
+        price={item.price}
+        rating={item.avgRating}
+        isOnSale={item.isOnSale}
+        image={item.imageUrl}
+        key={item._id}
+      />
     )
-  }
+  })
+  return (
+    <div className="catalog">
+      {items.length > 0 ? (
+        items
+      ) : (
+        <div className="message">{props.message}</div>
+      )}
+    </div>
+  )
 }
 
 Catalog.defaultProps = {
--- src/Components/Rating.js
+++ src/Components/Rating.js
@@ -1,26 +1,24 @@
-import React, { Component } from "react"
+import React from "react"
 import PropTypes from "prop-types"
 import FullStar from "../star_full.svg"
 import HalfStar from "../star_half.svg"
 import EmptyStar from "../star_empty.svg"
 
-export default class Rating extends Component {
-  render() {
-    let stars = []
-    let current = this.props.score
-    for (let i = 0; i < 5; i++) {
-      if (current >= 1) {
-        stars.push(<img src={FullStar} alt="" key={i} />)
-        current--
-      } else if (current === 0.5) {
-        stars.push(<img src={HalfStar} alt="" key={i} />)
-        current--
-      } else {
-        stars.push(<img src={EmptyStar} alt="" key={i} />)
-      }
+export default function Rating(props) {
+  let stars = []
+  let current = props.score
+  for (let i = 0; i < 5; i++) {
+    if (current >= 1) {
+      stars.push(<img src={FullStar} alt="" key={i} />)
+      current--
+    } else if (current === 0.5) {
+      stars.push(<img src={HalfStar} alt="" key={i} />)
+      current--
+    } else {
+      stars.push(<img src={EmptyStar} alt="" key={i} />)
     }
-    return <div className="rating">{stars}</div>
   }
+  return <div className="rating">{stars}</div>
 }
 
 Rating.defaultProps = {
