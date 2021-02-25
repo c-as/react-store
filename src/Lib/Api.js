1181b7dc1e0d9fd6afba781b10f78c742d39ad11
cbarsugman <casbrugman@hotmail.nl>
2021-02-25 15:29:06 +0100

    Updated to use try catch and removed unused isDeal function

--- src/Lib/Api.js
+++ src/Lib/Api.js
@@ -1,19 +1,31 @@
 const api = {
   url: "https://gp-super-store-api.herokuapp.com",
   async getItems() {
-    let response = await fetch(`${this.url}/item/list`)
-    return (await response.json()).items
+    try {
+      let response = await fetch(`${this.url}/item/list`)
+      return (await response.json()).items
+    } catch (error) {
+      console.error(error)
+      return {}
+    }
   },
   async getDeals() {
-    let response = await fetch(`${this.url}/item/list?isOnSale=true`)
-    return (await response.json()).items
+    try {
+      let response = await fetch(`${this.url}/item/list?isOnSale=true`)
+      return (await response.json()).items
+    } catch (error) {
+      console.error(error)
+      return {}
+    }
   },
   async getItem(id) {
-    let response = await fetch(`${this.url}/item/${id}`)
-    return await response.json()
-  },
-  isDeal(item) {
-    return item.isOnSale
+    try {
+      let response = await fetch(`${this.url}/item/${id}`)
+      return await response.json()
+    } catch (error) {
+      console.error(error)
+      return {}
+    }
   },
 }
 
