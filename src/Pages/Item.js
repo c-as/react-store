05cac5411734a332518d84ab423bb424951b1d92
cbarsugman <casbrugman@hotmail.nl>
2021-03-01 20:36:46 +0100

    Finalized item page

--- src/Pages/Item.js
+++ src/Pages/Item.js
@@ -2,15 +2,58 @@ import React, { useState, useEffect } from "react"
 import { withRouter } from "react-router-dom"
 import Api from "../Lib/Api"
 import styled from "styled-components"
+import Rating from "../Components/Rating"
+import Sale from "../Components/Sale"
+import ColorBox from "../Components/ColorBox"
 
 const Styled = styled.div`
-  width: 95rem;
+  width: 60rem;
   max-width: 100%;
-  margin: 1rem auto;
+  margin: 5rem auto;
+`
+
+const ProductImg = styled.img`
+  width: 50%;
+  float: left;
+`
+
+const Info = styled.div`
+  width: 50%;
+  float: right;
+
+  input {
+    margin-right: 1rem;
+  }
+`
+
+const Button = styled.div`
+  text-align: center;
+  width: 6rem;
+  color: white;
+  background-color: orange;
+  padding: 0.4rem;
+  border-radius: 0.2rem;
+  text-decoration: none;
+  cursor: pointer;
+`
+
+const StockWarning = styled(ColorBox)`
+  background-color: lightpink;
+`
+
+const CartWarning = styled(ColorBox)`
+  background-color: lightblue;
+`
+
+const Error = styled(ColorBox)`
+  background-color: lightpink;
+  margin: 0rem auto;
+  width: fit-content;
 `
 
 function Item(props) {
-  const [item, setItem] = useState({})
+  const [item, setItem] = useState()
+  const [quantity, setQuantity] = useState(1)
 
   useEffect(
     function () {
@@ -20,7 +63,7 @@ function Item(props) {
           setItem(item)
           console.log(item)
         } catch (error) {
-          setItem({})
+          setItem()
         }
       }
 
@@ -29,12 +72,49 @@ function Item(props) {
     [props.match.params.id]
   )
 
+  function quantityChanged(event) {
+    const { value } = event.target
+    console.log(event)
+    setQuantity(value)
+  }
+
+  function addToCart(event) {
+    alert(`${quantity} added to cart.`)
+  }
+
   return (
     <Styled>
-      <img src={item.imageUrl} alt={item.name} />
-      <div className="info">
-        <h2>{item.name}</h2>
-      </div>
+      {item ? (
+        <div>
+          <ProductImg src={item.imageUrl} alt={item.name} />
+          <Info>
+            <h2>{item.name}</h2>
+            {item.isOnSale && <Sale>On Sale</Sale>}
+            <Rating score={item.avgRating} />
+            <p>{item.description}</p>
+            <h3>${item.price}</h3>
+            <p>
+              Quantity:{" "}
+              <input
+                type="number"
+                min="1"
+                max={item.stockCount}
+                value={quantity}
+                onChange={(event) => quantityChanged(event)}
+                step="1"
+              />
+              In stock: {item.stockCount}
+            </p>
+            <Button onClick={(event) => addToCart(event)}>Add to cart</Button>
+            {(item.stockCount < 1 || quantity > item.stockCount) && (
+              <StockWarning>Insufficient stock!</StockWarning>
+            )}
+            <CartWarning>1 of this item is currently in your cart.</CartWarning>
+          </Info>
+        </div>
+      ) : (
+        <Error>Could not fetch item</Error>
+      )}
     </Styled>
   )
 }
