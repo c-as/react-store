49f10f9f0f6b7460dd0f86060fa1fad770c45c0a
cbarsugman <casbrugman@hotmail.nl>
2021-03-01 20:35:29 +0100

    Doesnt have to be component

--- src/Components/Sale.js
+++ src/Components/Sale.js
@@ -1,17 +1,10 @@
-import React from "react"
 import styled from "styled-components"
+import ColorBox from "./ColorBox"
 
-const Styled = styled.div`
+const Sale = styled(ColorBox)`
   width: fit-content;
-  float: inline-start;
-  font-size: 0.8rem;
-  margin: 0rem 0.5rem 0rem 0rem;
   background-color: red;
   color: white;
-  padding: 0.2rem;
-  border-radius: 0.15rem;
 `
 
-export default function Sale(props) {
-  return <Styled>{props.children}</Styled>
-}
+export default Sale
