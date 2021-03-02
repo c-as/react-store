01a602f6943d533f6ea39e8069ad8a8e5be7dea3
cbarsugman <casbrugman@hotmail.nl>
2021-03-02 19:59:27 +0100

    Added search bar to home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from "react"
+import { withRouter } from "react-router-dom"
 import Catalog from "../Components/Catalog/Catalog"
 import Api from "../Lib/Api"
+import SearchBar from "../Components/SearchBar"
 
-export default function Home() {
+function Home(props) {
   const [items, setItems] = useState([])
   const [errorMsg, setErrorMsg] = useState()
   const [isLoading, setLoading] = useState(true)
+
+  function onSearch(query) {
+    props.history.push(`/search?q=${query}`)
+  }
 
   useEffect(function () {
     async function fetchData() {
@@ -24,7 +30,10 @@ export default function Home() {
 
   return (
     <div>
+      <SearchBar onSearch={onSearch} />
       <Catalog items={items} message={errorMsg} isLoading={isLoading} />
     </div>
   )
 }
+
+export default withRouter(Home)
