b2988eebdb9d8fd694290db0866558af47dcb4bb
cbarsugman <casbrugman@hotmail.nl>
2021-03-11 15:14:28 +0100

    Readded api functions
    Cant use hooks for everything

--- src/Lib/Api.js
+++ src/Lib/Api.js
@@ -1,1 +1,30 @@
 export const url = "https://gp-super-store-api.herokuapp.com"
+
+async function _fetch(query) {
+  try {
+    const response = await fetch(query)
+    const json = await response.json()
+    return json
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+async function fetchList(params) {
+  try {
+    return _fetch(`${url}/item/list/${params}`)
+  } catch (error) {
+    throw error
+  }
+}
+
+async function fetchItem(id) {
+  try {
+    return _fetch(`${url}/item/${id}`)
+  } catch (error) {
+    throw error
+  }
+}
+
+export { fetchList }
