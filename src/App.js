5474c3b9babc9ef9f8c6de472c78be9ca6248095
cbarsugman <casbrugman@hotmail.nl>
2021-03-11 14:24:32 +0100

    Add cart context and cart page to app

--- src/App.js
+++ src/App.js
@@ -3,27 +3,33 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Header from "./Components/Header"
 import Item from "./Pages/Item"
 import List from "./Pages/List"
+import Cart from "./Pages/Cart"
+import { Provider as CartProvider } from "./Context/Cart"
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <List />
-        </Route>
-        <Route path="/cart">Your cart</Route>
-        <Route path="/item/:id">
-          <Item />
-        </Route>
-        <Route path="/list">
-          <List />
-        </Route>
-        <Route>
-          <h1 style={{ textAlign: "center" }}>404 not found</h1>
-        </Route>
-      </Switch>
-    </Router>
+    <CartProvider>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <List />
+          </Route>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+          <Route path="/item/:id">
+            <Item />
+          </Route>
+          <Route path="/list">
+            <List />
+          </Route>
+          <Route>
+            <h1 style={{ textAlign: "center" }}>404 not found</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </CartProvider>
   )
 }
 
