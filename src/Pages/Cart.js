5c33b84fd5808fd86322cf1a7da1592d3057c5b1
cbarsugman <casbrugman@hotmail.nl>
2021-03-11 16:46:45 +0100

    Catalog on cart page to display items in cart

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,8 +1,50 @@
-import React, { useContext } from "react"
+import React, { useContext, useState, useEffect } from "react"
 import { Context as CartContext } from "../Context/Cart"
+import Catalog from "../Components/Catalog"
+import CatalogItem from "../Components/CatalogItem"
+import { fetchItem } from "../Lib/Api"
 
 export default function Cart() {
-  const { items } = useContext(CartContext)
+  const { items: cart } = useContext(CartContext)
 
-  return <div>{JSON.stringify(items)}</div>
+  const [items, setItems] = useState()
+  const [isLoading, setIsLoading] = useState(true)
+  const [message, setMessage] = useState("")
+
+  useEffect(
+    function () {
+      async function getItems() {
+        setIsLoading(true)
+        setMessage("")
+        try {
+          let list = []
+
+          for (const id in cart) {
+            console.log(id)
+            const item = await fetchItem(id)
+            list.push(item)
+          }
+          setItems(list)
+        } catch (error) {
+          setMessage(error.toString())
+        } finally {
+          setIsLoading(false)
+        }
+      }
+      getItems()
+    },
+    [cart]
+  )
+
+  return (
+    <div>
+      {JSON.stringify(cart)}
+      <Catalog
+        items={items}
+        ItemElement={CatalogItem}
+        isLoading={isLoading}
+        message={message}
+      />
+    </div>
+  )
 }
