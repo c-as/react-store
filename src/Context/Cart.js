44b9316fc424a31ae631e3e98944e10b8b81908d
cbarsugman <casbrugman@hotmail.nl>
2021-03-12 15:03:32 +0100

    Added setItem function

--- src/Context/Cart.js
+++ src/Context/Cart.js
@@ -5,20 +5,32 @@ export const Context = createContext()
 export function Provider(props) {
   const [items, setItems] = useState({})
 
-  function updateItem(item, amount) {
+  function setItem(item, amount) {
     const id = item._id
-    const newAmount = (items[id] || 0) + amount
-
-    if (amount === 0 || newAmount > item.stockCount) {
+    if (amount > item.stockCount || item.stockCount === 0) {
       throw Error("Insufficient Stock")
     }
     setItems((items) => {
-      return { ...items, [id]: newAmount }
+      return { ...items, [id]: amount }
+    })
+  }
+
+  function updateItem(item, amount) {
+    const id = item._id
+    const newAmount = (items[id] || 0) + amount
+    setItem(item, newAmount)
+  }
+
+  function removeItem(item) {
+    const id = item._id
+    setItems((items) => {
+      delete items[id]
+      return items
     })
   }
 
   return (
-    <Context.Provider value={{ items, setItems, updateItem }}>
+    <Context.Provider value={{ items, setItem, updateItem, removeItem }}>
       {props.children}
     </Context.Provider>
   )
