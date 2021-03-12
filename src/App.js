329197e89bb90cf77afde947d54816d3aa946188
cbarsugman <casbrugman@hotmail.nl>
2021-03-12 19:24:47 +0100

    Created checkout page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 import Header from "./Components/Header"
 import Item from "./Pages/Item"
 import List from "./Pages/List"
 import Cart from "./Pages/Cart"
+import { Button } from "./Components/Styles"
 import { Provider as CartProvider } from "./Context/Cart"
 
 function App() {
@@ -23,6 +24,12 @@ function App() {
           </Route>
           <Route path="/list">
             <List />
+          </Route>
+          <Route path="/checkout">
+            <h2 style={{ textAlign: "center" }}>Thank you</h2>
+            <Button style={{ margin: "auto" }}>
+              <Link to="/">Return to the home page </Link>
+            </Button>
           </Route>
           <Route>
             <h1 style={{ textAlign: "center" }}>404 not found</h1>
