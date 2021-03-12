9e774e3424cdc11f82b613060e53973afcca223c
cbarsugman <casbrugman@hotmail.nl>
2021-03-12 18:58:45 +0100

    Use cartItem

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,15 +1,33 @@
-import React, { useContext, useState, useEffect } from "react"
+import React, { useContext, useState, useEffect, useMemo } from "react"
 import { Context as CartContext } from "../Context/Cart"
 import Catalog from "../Components/Catalog"
-import CatalogItem from "../Components/CatalogItem"
+import CartItem from "../Components/CartItem"
 import { fetchItem } from "../Lib/Api"
+import styled from "styled-components"
+import { ColorBox } from "../Components/Styles"
+
+const Button = styled(ColorBox)`
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+  width: max-content;
+  margin: 2rem auto 0rem;
+  background-color: orange;
+  color: white;
+  cursor: pointer;
+`
 
 export default function Cart() {
-  const { items: cart } = useContext(CartContext)
+  const { items: cart, setItem } = useContext(CartContext)
 
-  const [items, setItems] = useState()
+  const [items, setItems] = useState({})
   const [isLoading, setIsLoading] = useState(true)
   const [message, setMessage] = useState("")
+
+  //DEBUG
+  useEffect(() => {
+    setItem({ _id: "5fbfff7d58aa65167efb52b5" }, 1)
+    setItem({ _id: "5fbfff7d58aa65167efb52af" }, 2)
+  }, [])
 
   useEffect(
     function () {
@@ -17,14 +35,25 @@ export default function Cart() {
         setIsLoading(true)
         setMessage("")
         try {
-          let list = []
-
+          let list = {}
           for (const id in cart) {
-            console.log(id)
+            if (!cart[id] > 0) {
+              continue
+            }
+
+            if (items[id]) {
+              list[id] = items[id]
+              continue
+            }
+
             const item = await fetchItem(id)
-            list.push(item)
+            list[id] = item
           }
           setItems(list)
+
+          if (!Object.keys(list).length > 0) {
+            setMessage("Cart is empty")
+          }
         } catch (error) {
           setMessage(error.toString())
         } finally {
@@ -36,15 +65,31 @@ export default function Cart() {
     [cart]
   )
 
+  const price = useMemo(
+    function () {
+      if (!items) {
+        return 0
+      }
+
+      let price = 0
+      Object.values(items).forEach((item) => {
+        price += item.price
+      })
+      return price
+    },
+    [items]
+  )
+
   return (
     <div>
-      {JSON.stringify(cart)}
       <Catalog
-        items={items}
-        ItemElement={CatalogItem}
+        items={Object.values(items)}
+        ItemElement={CartItem}
         isLoading={isLoading}
         message={message}
       />
+      {Object.values(items).length > 0 && <Button>Checkout</Button>}
+      <div>Total: ${price}</div>
     </div>
   )
 }
